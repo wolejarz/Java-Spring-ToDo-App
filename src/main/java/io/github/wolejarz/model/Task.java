6beb636a83684921cc9f68f3d9b2cb262083e539
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-03-09 22:50:29 +0100

    Class Task created

--- src/main/java/io/github/wolejarz/model/Task.java
+++ src/main/java/io/github/wolejarz/model/Task.java
@@ -1,7 +1,37 @@
 package io.github.wolejarz.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 public class Task {
+    @Id
+    private int id;
+    private String description;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
