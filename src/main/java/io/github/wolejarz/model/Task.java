acb001da69a1c703d0d1aff35289b0cc35c720a6
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-03-09 20:28:10 +0100

    first commit

---
+++ src/main/java/io/github/wolejarz/model/Task.java
@@ -0,0 +1,39 @@
+package io.github.wolejarz.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Task's description must be not null or empty")
+    private String description;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
