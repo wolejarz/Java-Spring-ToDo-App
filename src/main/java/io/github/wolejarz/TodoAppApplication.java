dfd354e3da78b81fabde304422f4fbcbfc169b10
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-03-11 22:13:49 +0100

    Controller changed

--- src/main/java/io/github/wolejarz/TodoAppApplication.java
+++ src/main/java/io/github/wolejarz/TodoAppApplication.java
@@ -2,6 +2,11 @@ package io.github.wolejarz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Validator;
+
 
 @SpringBootApplication
 public class TodoAppApplication {
@@ -10,4 +15,6 @@ public class TodoAppApplication {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
+	@Bean
+	Validator validator() {return new LocalValidatorFactoryBean();};
 }
--- src/main/java/io/github/wolejarz/controller/TaskController.java
+++ src/main/java/io/github/wolejarz/controller/TaskController.java
@@ -4,15 +4,17 @@ import io.github.wolejarz.model.Task;
 import io.github.wolejarz.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RepositoryRestController
+@RestController
 public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -20,9 +22,15 @@ public class TaskController {
     public TaskController(TaskRepository repository) {
         this.repository = repository;
     }
-    @GetMapping("/tasks")
-    ResponseEntity <?> readAllTasks() {
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity <List<Task>> readAllTasks() {
         logger.warn("Exposing All Records");
         return  ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity <List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return  ResponseEntity.ok(repository.findAll(page).getContent());
     }
 }
